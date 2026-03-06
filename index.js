e21087c16727d1b32ba72d282a72d9ef690a3b55
guptaprerna2006-del <guptaprerna2006@gmail.com>
2026-03-06 15:00:23 +0530

    fix vercel ejs path

--- index.js
+++ index.js
@@ -2,14 +2,20 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import studentRoutes from "./routes/student.routes.js";
+import path from "path";
+import { fileURLToPath } from "url";
 
 dotenv.config();
 connectDB();
 
 const app = express();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 app.use("/students", studentRoutes);
 
@@ -17,5 +23,4 @@ app.get("/", (req, res) => {
   res.redirect("/students");
 });
 
-// DON'T use app.listen for Vercel
 export default app;
